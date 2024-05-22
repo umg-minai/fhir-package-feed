32db641944b3ab61dc867c5d3ba3a02e61f70a4b
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2024-05-22 17:28:32 +0200

    ci: add xml validator

---
+++ validate_package_feed.py
@@ -0,0 +1,14 @@
+import lxml.etree as ET
+
+schema_doc = ET.parse('schema/package-feed-schema.xsd')
+schema = ET.XMLSchema(schema_doc)
+
+xml_doc = ET.parse('package-feed.xml')
+
+if not schema.validate(xml_doc):
+    for error in schema.error_log:
+        print(f"Error ({error.filename}:{error.line}): {error.message}")
+        #print(dir(error))
+    raise Exception("XML validation failed")
+else:
+    print("XML is valid")
